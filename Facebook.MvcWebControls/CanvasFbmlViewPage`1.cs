02d93f87cf88bd0a6da2e7329905d6b55632310e
nberardi <nberardi@7eb1aee6-a419-0410-b45c-7b515150b9e3>
2008-04-11 15:13:28 +0000

    added facebook controls for MVC

---
+++ Facebook.MvcWebControls/CanvasFbmlViewPage`1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Web;
+
+namespace Facebook.MvcWebControls
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="TViewData">The type of the view data.</typeparam>
+	[AspNetHostingPermission(System.Security.Permissions.SecurityAction.LinkDemand, Level = AspNetHostingPermissionLevel.Minimal)]
+	[AspNetHostingPermission(System.Security.Permissions.SecurityAction.InheritanceDemand, Level = AspNetHostingPermissionLevel.Minimal)]
+	public class CanvasFbmlViewPage<TViewData> : CanvasFbmlViewPage
+	{
+		private TViewData _viewData;
+
+		/// <summary>
+		/// Gets the view data.
+		/// </summary>
+		/// <value>The view data.</value>
+		public new TViewData ViewData
+		{
+			get { return _viewData; }
+		}
+
+		/// <summary>
+		/// Sets the view data.
+		/// </summary>
+		/// <param name="viewData">The view data.</param>
+		protected override void SetViewData(object viewData)
+		{
+			if (viewData != null && !(viewData is TViewData))
+			{
+				throw new ArgumentException(
+					String.Format(
+						CultureInfo.CurrentUICulture,
+						"The view data passed into the page is of type '{0}' but this page requires view data of type '{1}'.",
+						viewData.GetType(),
+						typeof(TViewData)),
+					"viewData");
+			}
+
+			_viewData = (TViewData)viewData;
+			base.SetViewData(viewData);
+		}
+	}
+}
---
+++ Facebook.MvcWebControls/CanvasIFrameViewMasterPage.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Web;
+using System.Web.Mvc;
+using Facebook.Components;
+
+namespace Facebook.MvcWebControls
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[AspNetHostingPermission(System.Security.Permissions.SecurityAction.LinkDemand, Level = AspNetHostingPermissionLevel.Minimal)]
+	[AspNetHostingPermission(System.Security.Permissions.SecurityAction.InheritanceDemand, Level = AspNetHostingPermissionLevel.Minimal)]
+	public class CanvasIFrameViewMasterPage : ViewMasterPage
+	{
+		FacebookService _fbService = new Facebook.Components.FacebookService();
+		private string _api = null;
+		private string _secret = null;
+		private bool _useSession = true;
+		private bool _autoAdd = true;
+
+		/// <summary>
+		/// Gets or sets the API.
+		/// </summary>
+		/// <value>The API.</value>
+		public string Api
+		{
+			get { return _api; }
+			set { _api = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the secret.
+		/// </summary>
+		/// <value>The secret.</value>
+		public string Secret
+		{
+			get { return _secret; }
+			set { _secret = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets a value indicating whether [use session].
+		/// </summary>
+		/// <value>
+		/// 	<see langword="true"/> if [use session]; otherwise, <see langword="false"/>.
+		/// </value>
+		public bool UseSession
+		{
+			get { return _useSession; }
+			set { _useSession = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets a value indicating whether [auto add].
+		/// </summary>
+		/// <value>
+		/// 	<see langword="true"/> if [auto add]; otherwise, <see langword="false"/>.
+		/// </value>
+		public bool AutoAdd
+		{
+			get { return _autoAdd; }
+			set { _autoAdd = value; }
+		}
+
+		/// <summary>
+		/// Gets the FB service.
+		/// </summary>
+		/// <value>The FB service.</value>
+		public Facebook.Components.FacebookService FBService
+		{
+			get { return _fbService; }
+		}
+
+		/// <summary>
+		/// Raises the <see cref="E:System.Web.UI.Control.PreRender"/> event.
+		/// </summary>
+		/// <param name="e">An <see cref="T:System.EventArgs"/> object that contains the event data.</param>
+		protected override void OnPreRender(EventArgs e)
+		{
+			Response.AppendHeader("P3P", "CP=\"CAO PSA OUR\"");
+			base.OnPreRender(e);
+		}
+
+		/// <summary>
+		/// Handles the Init event of the Page control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		protected void Page_Init(object sender, EventArgs e)
+		{
+			// ApplicationKey and Secret are acquired when you sign up for 
+			_fbService.ApplicationKey = _api;
+			_fbService.Secret = _secret;
+
+			BasePageHelper.LoadIFramePage(_fbService, _useSession, _autoAdd, Request, Response, Session);
+		}
+	}
+}
