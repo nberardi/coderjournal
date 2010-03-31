b09bbbbf0a93d78ce65f48d8b9dc6833aca478a0
nberardi <nberardi@7eb1aee6-a419-0410-b45c-7b515150b9e3>
2010-03-31 01:28:36 +0000

    post for thrift interfaces to Cassandra

---
+++ trunk/Posts/2010/03/gen-xsd/cassandra_xsd.php
@@ -0,0 +1,9 @@
+<?php
+$GLOBALS['cassandra_xsd_elt_columns'] = 'Column';
+$GLOBALS['cassandra_xsd_elt_column_names'] = 'column_names_elt';
+$GLOBALS['cassandra_xsd_elt_columns'] = 'ColumnOrSuperColumn';
+$GLOBALS['cassandra_xsd_elt_get_slice_response'] = 'ColumnOrSuperColumn';
+$GLOBALS['cassandra_xsd_elt_get_key_range_response'] = 'get_key_range_response_elt';
+$GLOBALS['cassandra_xsd_elt_get_range_slice_response'] = 'KeySlice';
+$GLOBALS['cassandra_xsd_elt_get_string_list_property_response'] = 'get_string_list_property_response_elt';
+?>
